2dcd9bb562536e386d5026c7fa1d41310bb87bff
alpha1729 <bhaskarbnms0@gmail.com>
2025-11-17 23:35:42 +0530

    custom format with loglevel log

--- env-logger/src/main.rs
+++ env-logger/src/main.rs
@@ -1,8 +1,23 @@
-use env_logger;
+use env_logger::Builder;
 use log::{ debug, info, warn, error};
+use std::io::Write;
 
 fn main() {
-    env_logger::init();
+    // env_logger::init();
+    Builder::new()
+        .filter(None, log::LevelFilter::Info) // now even with RUST_LOG=debug, only info and above will be shown
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} [{}] - {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%s"),
+                record.level(),
+                record.args()
+            )
+        })
+        .init();
+
+    
     info!("This is an info message");
     warn!("This is a warning message");
     error!("This is an error message");
